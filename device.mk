df595214b7812ca179bdc4d602d1aaf5303098ad
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    beryllium: Remove OMX audio codecs
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- device.mk
+++ device.mk
@@ -322,12 +322,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
