aeb99c7d1db72c7173799105eac1e0499d756a4e
Rve27 <rve27github@gmail.com>
2025-02-28 12:30:04 +0700

    beryllium: Use hardware/xiaomi FCM
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,8 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-     $(DEVICE_PATH)/configs/hidl/xiaomi_vendor_framework_compatibility_matrix.xml
+     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
      $(DEVICE_PATH)/configs/hidl/manifest.xml
